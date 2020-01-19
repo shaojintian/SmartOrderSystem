dff18695cee4016babd7eb16bda39cbfaacc25fd
shaojintian <sjt@hnu.edu.cn>
2020-01-20 00:01:41 +0800

    fix entity bug

--- src/main/java/cn/shaojintian/smartordersys/model/BaseEntity.java
+++ src/main/java/cn/shaojintian/smartordersys/model/BaseEntity.java
@@ -1,16 +1,19 @@
 package cn.shaojintian.smartordersys.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
-
+@MappedSuperclass
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BaseEntity implements Serializable {
 
     @Id
